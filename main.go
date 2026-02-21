27854cfb22e1239933ba4aef02aa03f4f0a5a295
“arthurnagem” <arthurnagem24@gmail.com>
2026-02-21 13:53:15 -0300

    implement extensible CLI command system with login command

--- main.go
+++ main.go
@@ -2,29 +2,79 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
 
 	"github.com/arthurnagem/gator/internal/config"
 )
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlers[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, exists := c.handlers[cmd.name]
+	if !exists {
+		return fmt.Errorf("unknown command: %s", cmd.name)
+	}
+	return handler(s, cmd)
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("username is required")
+	}
+
+	username := cmd.args[0]
+
+	if err := s.cfg.SetUser(username); err != nil {
+		return err
+	}
+
+	fmt.Printf("User set to %s\n", username)
+	return nil
+}
 
 func main() {
-	// 1. Read config
+	if len(os.Args) < 2 {
+		fmt.Println("not enough arguments")
+		os.Exit(1)
+	}
+
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	// 2. Set user
-	err = cfg.SetUser("arthurnagem")
-	if err != nil {
-		log.Fatal(err)
+	s := &state{
+		cfg: &cfg,
 	}
 
-	// 3. Read again
-	updatedCfg, err := config.Read()
-	if err != nil {
-		log.Fatal(err)
+	cmds := &commands{
+		handlers: make(map[string]func(*state, command) error),
 	}
 
-	fmt.Printf("Config contents:\n%+v\n", updatedCfg)
+	cmds.register("login", handlerLogin)
+
+	cmd := command{
+		name: os.Args[1],
+		args: os.Args[2:],
+	}
+
+	if err := cmds.run(s, cmd); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
