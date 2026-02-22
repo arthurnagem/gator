41d6fb470a03fef8fa9000c2bf13192fc2a32319
“arthurnagem” <arthurnagem24@gmail.com>
2026-02-21 21:46:30 -0300

    implement register and login with postgres integration via sqlc

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
-
+	"time"
+	"database/sql"
+    _ "github.com/lib/pq"
+	"github.com/google/uuid"
 	"github.com/arthurnagem/gator/internal/config"
+	"github.com/arthurnagem/gator/internal/database"
 )
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -38,6 +44,12 @@ func handlerLogin(s *state, cmd command) error {
 
 	username := cmd.args[0]
 
+	_, err := s.db.GetUser(context.Background(), username)
+	if err != nil {
+		fmt.Println("user does not exist")
+		os.Exit(1)
+	}
+
 	if err := s.cfg.SetUser(username); err != nil {
 		return err
 	}
@@ -45,6 +57,39 @@ func handlerLogin(s *state, cmd command) error {
 	fmt.Printf("User set to %s\n", username)
 	return nil
 }
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("username is required")
+	}
+
+	name := cmd.args[0]
+
+	user, err := s.db.CreateUser(
+		context.Background(),
+		database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			Name:      name,
+		},
+	)
+
+	if err != nil {
+		fmt.Println("user already exists")
+		os.Exit(1)
+	}
+
+	if err := s.cfg.SetUser(name); err != nil {
+		return err
+	}
+
+	fmt.Println("User created successfully:")
+	fmt.Println(user)
+
+	return nil
+}
+
 
 func main() {
 	if len(os.Args) < 2 {
@@ -58,7 +103,16 @@ func main() {
 		os.Exit(1)
 	}
 
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+	fmt.Println(err)
+	os.Exit(1)
+	}
+
+	dbQueries := database.New(db)
+
 	s := &state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 
@@ -67,6 +121,8 @@ func main() {
 	}
 
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+
 
 	cmd := command{
 		name: os.Args[1],
